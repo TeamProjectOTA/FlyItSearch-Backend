551982516c8342b68c8caedc0b9c1e288cca1695
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2024-06-23 15:40:15 +0600

    Create airports.controller.d.ts

---
+++ dist/airports/airports.controller.d.ts
@@ -0,0 +1,12 @@
+import { AirportsService } from './airports.service';
+import { AirportsModel, AirportsModelUpdate } from './airports.model';
+export declare class AirportsController {
+    private readonly airportsService;
+    constructor(airportsService: AirportsService);
+    create(createAirportDto: AirportsModel): Promise<AirportsModel>;
+    findAll(): Promise<AirportsModel[]>;
+    findFormateAll(): Promise<AirportsModel[]>;
+    findOne(id: string): Promise<AirportsModel>;
+    update(id: string, updateAirportDto: AirportsModelUpdate): Promise<import("typeorm").UpdateResult>;
+    remove(id: string): Promise<AirportsModel[]>;
+}
