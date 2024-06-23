b6af8cfc5b173e475786980925f274cb5b5c2c98
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2024-06-23 15:39:36 +0600

    Create airlines.model.d.ts

---
+++ dist/airlines/airlines.model.d.ts
@@ -0,0 +1,43 @@
+export declare class AirlinesModel {
+    id: number;
+    iata: string;
+    soto: number;
+    soti: number;
+    sito: number;
+    domestic: number;
+    addAmount: number;
+    instantPayment: boolean;
+    issuePermit: string;
+    isBlocked: boolean;
+    bookable: boolean;
+    docs: string;
+    icao: string;
+    marketing_name: string;
+    full_name: string;
+    status: string;
+    type: string;
+    alliance: string;
+    ffpName: string;
+    lowCost: string;
+    countryName: string;
+    founded: string;
+    baggageUrl: string;
+    website: string;
+    webCheckinUrl: string;
+    mobileCheckinUrl: string;
+    logo: string;
+    createdAt: string;
+    updatedAt: string;
+    uid: string;
+}
+export declare class AirlinesUpdateModel {
+    soto: number;
+    soti: number;
+    sito: number;
+    domestic: number;
+    addAmount: number;
+    instantPayment: boolean;
+    issuePermit: string;
+    isBlocked: boolean;
+    bookable: boolean;
+}
