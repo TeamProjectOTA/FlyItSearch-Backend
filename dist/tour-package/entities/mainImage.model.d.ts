b53aeaf8468ad14f522fdd2510dc0b5cf8ce5e06
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2024-06-24 17:24:36 +0600

    Create mainImage.model.d.ts

---
+++ dist/tour-package/entities/mainImage.model.d.ts
@@ -0,0 +1,8 @@
+import { TourPackage } from './tour-package.entity';
+export declare class MainImage {
+    id: number;
+    path: string;
+    size: number;
+    mainTitle: string;
+    tourPackage: TourPackage;
+}
