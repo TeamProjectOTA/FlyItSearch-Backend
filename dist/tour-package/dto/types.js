64ae3e0372c996362e1443eb563bd93b7f160c61
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2024-06-24 17:24:25 +0600

    Update types.js

--- dist/tour-package/dto/types.js
+++ dist/tour-package/dto/types.js
@@ -9,7 +9,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.TravelPackageInclusionDto = exports.PackageInclude = exports.TripType = void 0;
+exports.RefundPolicy = exports.BookingPolicy = exports.Exclusion = exports.Inclusion = exports.TravelPackageInclusionDto = exports.PackageInclude = exports.TripType = void 0;
 const swagger_1 = require("@nestjs/swagger");
 var TripType;
 (function (TripType) {
@@ -35,4 +35,32 @@ __decorate([
     (0, swagger_1.ApiProperty)({ default: 'Details about the inclusion.' }),
     __metadata("design:type", String)
 ], TravelPackageInclusionDto.prototype, "details", void 0);
+class Inclusion {
+}
+exports.Inclusion = Inclusion;
+__decorate([
+    (0, swagger_1.ApiProperty)({ default: 'This is the Inclusion' }),
+    __metadata("design:type", String)
+], Inclusion.prototype, "data", void 0);
+class Exclusion {
+}
+exports.Exclusion = Exclusion;
+__decorate([
+    (0, swagger_1.ApiProperty)({ default: 'This is the Exclusion' }),
+    __metadata("design:type", String)
+], Exclusion.prototype, "data", void 0);
+class BookingPolicy {
+}
+exports.BookingPolicy = BookingPolicy;
+__decorate([
+    (0, swagger_1.ApiProperty)({ default: 'This is the booking policy' }),
+    __metadata("design:type", String)
+], BookingPolicy.prototype, "data", void 0);
+class RefundPolicy {
+}
+exports.RefundPolicy = RefundPolicy;
+__decorate([
+    (0, swagger_1.ApiProperty)({ default: 'This is the Refund Policy' }),
+    __metadata("design:type", String)
+], RefundPolicy.prototype, "data", void 0);
 //# sourceMappingURL=types.js.map
