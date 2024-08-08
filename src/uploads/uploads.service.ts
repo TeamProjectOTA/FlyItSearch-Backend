5861cc5e3b0977e184992eb9d743d0c533b7b851
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2024-08-08 16:44:07 +0600

    Create uploads.service.ts

---
+++ src/uploads/uploads.service.ts
@@ -0,0 +1,84 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProfilePicture } from './uploads.model';
+import { Repository } from 'typeorm';
+import { User } from 'src/user/entities/user.entity';
+
+import { extname, join } from 'path';
+import { promises as fs } from 'fs';
+import { AuthService } from 'src/auth/auth.service';
+
+@Injectable()
+export class UploadsService {
+  constructor(
+    @InjectRepository(ProfilePicture)
+    private profilePictureRepository: Repository<ProfilePicture>,
+    private readonly authservice: AuthService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+
+ 
+  async create(header: any, file: Express.Multer.File): Promise<ProfilePicture> {
+    const decodeToken = await this.authservice.decodeToken(header);
+    const user = await this.userRepository.findOne({
+      where: { email: decodeToken },
+    });
+    
+    if (!user) {
+      throw new BadRequestException('User not found.');
+    }
+    const existingProfilePicture = await this.profilePictureRepository.findOne({
+      where: { user },
+    });
+
+    if (existingProfilePicture) {
+      try {
+        await fs.unlink(existingProfilePicture.path);
+      } catch (error) {
+        
+        //console.error('Failed to delete existing profile picture:', error);
+      }
+      
+      
+      await this.profilePictureRepository.remove(existingProfilePicture);
+    }
+
+ 
+    const fileExtension = extname(file.originalname);
+    const filename = `${user.passengerId}-ProfilePicture_of-${user.fullName}${fileExtension}`;
+    const path = join('uploads', filename);
+
+    try {
+      await fs.rename(file.path, path);
+    } catch (error) {
+      throw new BadRequestException('Failed to save file.');
+    }
+
+    const size = file.size;
+    const profilePicture = this.profilePictureRepository.create({
+      user,
+      filename,
+      path,
+      size,
+    });
+
+    return this.profilePictureRepository.save(profilePicture);
+  }
+  async delete(header: any): Promise<any> {
+    const decodeToken = await this.authservice.decodeToken(header);
+    const user = await this.userRepository.findOne({
+      where: { email: decodeToken },
+    });
+    const profilePicture = await this.profilePictureRepository.findOne({ where: { user } });
+
+    if (!profilePicture) {
+      throw new NotFoundException('Profile picture not found');
+    }
+
+    return await this.profilePictureRepository.remove(profilePicture);
+  }
+
+  }
+
