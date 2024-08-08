522d7803f4658478beeae5330150d56d0ccb1ca5
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2024-08-08 16:44:29 +0600

    Create hotdeals.module.ts

---
+++ src/homepage/hotdeals/hotdeals.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HotDeals } from './hotdeals.model';
+import { HotDealsService } from './hotdeals.service';
+import { HotDealsController } from './hotdeals.controller';
+
+
+@Module({
+  imports: [TypeOrmModule.forFeature([HotDeals])],
+  providers: [HotDealsService],
+  controllers: [HotDealsController],
+})
+export class HotDealsModule {}
