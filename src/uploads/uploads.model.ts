4c2f7a4eed0b67809ec725caa30bff4ee9de98c7
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2024-08-08 16:44:14 +0600

    Create uploads.model.ts

---
+++ src/uploads/uploads.model.ts
@@ -0,0 +1,27 @@
+import { User } from 'src/user/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class ProfilePicture {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  filename: string;
+
+  @Column()
+  path: string;
+
+  @Column()
+  size: number;
+
+  @OneToOne(() => User, (user) => user.profilePicture, { onDelete: 'CASCADE' })
+  @JoinColumn()
+  user: User;
+}
